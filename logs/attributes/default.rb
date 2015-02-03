039e87d38532ec781f9370cc99430fece7865bf9
Vu Nguyen <vnguyen@rec.ri.cmu.edu>
2015-02-02 22:57:38 -0500

    Editing the cloud watch log file settings to see if it logs the nginx errors.

--- logs/attributes/default.rb
+++ logs/attributes/default.rb
@@ -1,1 +1,2 @@
-default[:cwlogs][:logfile] = '/var/log/aws/opsworks/opsworks-agent.statistics.log'
+#default[:cwlogs][:logfile] = '/var/log/aws/opsworks/opsworks-agent.statistics.log'
+default[:cwlogs][:logfile] = '/var/log/nginx/error.log'
