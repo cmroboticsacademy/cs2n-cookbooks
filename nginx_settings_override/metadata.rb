b385964f071e289f1d580183a2392ac29cc5bdd7
Vu Nguyen <vnguyen@rec.ri.cmu.edu>
2015-02-02 22:55:55 -0500

    Putting the sysctl file-max setting into an customized attributes file

--- nginx_settings_override/metadata.rb
+++
@@ -1,4 +0,0 @@
-name        "nginx_settings_override"
-description "Override's nginx's maximum file connections"
-maintainer  "Vu Nguyen"
-version     "1.0.0"
--- nginx_settings_override/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-file "/etc/sysctl.conf" do
-  content <<-EOH
-  fs.file-max = 120000 
-  EOH
-end
-
-bash "run sysctl command" do
-  code "sysctl -p"
-end
