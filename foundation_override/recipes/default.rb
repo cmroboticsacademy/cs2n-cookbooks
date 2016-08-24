f60964660d207eee691e07f5c00f80a206316ed8
Vu Nguyen <vnguyen@robomatter.com>
2016-08-24 15:00:51 -0400

    Updating the foundation override so that it creates the directory if it doesn't exist.

--- foundation_override/recipes/default.rb
+++ foundation_override/recipes/default.rb
@@ -1,3 +1,12 @@
+config_dir = "/home/deploy/.bundler/cs2n/ruby/1.9.1/gems/foundation-rails-5.5.0.0/vendor/assets/stylesheets/foundation/"
+
+directory config_dir do
+  owner "deploy"
+  group "www-data"
+  mode 0644
+  recursive true
+end
+
 cookbook_file value_for_platform([ "centos", "redhat", "fedora", "suse" ] => {"default" => "/home/deploy/.bundler/cs2n/ruby/1.9.1/gems/foundation-rails-5.5.0.0/vendor/assets/stylesheets/foundation/_functions.scss"}, "default" => "/home/deploy/.bundler/cs2n/ruby/1.9.1/gems/foundation-rails-5.5.0.0/vendor/assets/stylesheets/foundation/_functions.scss") do
   source "_functions.scss"
   owner "deploy"
