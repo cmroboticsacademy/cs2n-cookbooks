9e28706989e6a2311c2e7607c4350b157a5cb1cf
Vu Nguyen <vnguyen@robomatter.com>
2017-05-04 11:01:28 -0400

    writing it another way because why not

--- sidekiq_cron/recipes/default.rb
+++ sidekiq_cron/recipes/default.rb
@@ -1,7 +1,4 @@
 cron "sidekiq_cronjob" do
   minute "*/5"
-  command %Q{
-    cd /srv/www/cs2n/current/ && 
-    sudo bundle exec rake job_processess:test
-  }
+  command "cd /srv/www/cs2n/current/ && sudo bundle exec rake job_processess:test"
 end
