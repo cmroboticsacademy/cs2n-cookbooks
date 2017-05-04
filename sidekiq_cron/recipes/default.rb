146cf075b8939d42f238a204911340903750af3a
Vu Nguyen <vnguyen@robomatter.com>
2017-05-04 11:18:27 -0400

    Trying with the rails env

--- sidekiq_cron/recipes/default.rb
+++ sidekiq_cron/recipes/default.rb
@@ -1,4 +1,4 @@
 cron "sidekiq_cronjob" do
   minute "*/5"
-  command "cd /srv/www/cs2n/current/ && sudo bundle exec rake job_processess:test"
+  command "cd /srv/www/cs2n/current/ && sudo RAILS_ENV=#{node["deploy"]["cs2n"]["rails_env"]} bundle exec rake job_processess:test"
 end
