dc6c07a197db10e2171f529223b52de233e33f82
Vu Nguyen <vnguyen@robomatter.com>
2017-05-04 11:47:56 -0400

    Adding mailto to see if it is doing something

--- sidekiq_cron/recipes/default.rb
+++ sidekiq_cron/recipes/default.rb
@@ -1,4 +1,5 @@
 cron "sidekiq_cronjob" do
   minute "*/5"
   command "cd /srv/www/cs2n/current/ && sudo RAILS_ENV=#{node["deploy"]["cs2n"]["rails_env"]} bundle exec rake job_processess:test"
+  mailto "vnguyen@nrec.ri.cmu.edu"
 end
