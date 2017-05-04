d9c1117effa369bc5ed42a84c2a260d0cfcec120
Vu Nguyen <vnguyen@robomatter.com>
2017-05-04 10:06:41 -0400

    combining commands?

--- sidekiq_cron/recipes/default.rb
+++ sidekiq_cron/recipes/default.rb
@@ -1,6 +1,5 @@
 cron "sidekiq" do
   minute "*/1"
-  command "cd /srv/www/cs2n/current/"
-  command "sudo bundle exec rake job_processess:test"
+  command "cd /srv/www/cs2n/current/ && sudo bundle exec rake job_processess:test"
   environment(  { 'RAILS_ENV' => node[:deploy]['cs2n'][:rails_env] } )
 end
