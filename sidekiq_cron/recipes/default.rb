064979c78e621acf7359107bf0ad8fd3236b5a69
Vu Nguyen <vnguyen@robomatter.com>
2017-05-04 10:31:42 -0400

    forgot about the shortname

--- sidekiq_cron/recipes/default.rb
+++ sidekiq_cron/recipes/default.rb
@@ -2,6 +2,6 @@ cron "sidekiq" do
   minute "*/1"
   command %Q{
     cd /srv/www/cs2n/current/ && 
-    sudo RAILS_ENV=#{node["deploy"]["appshortname"]["rails_env"]} bundle exec rake job_processess:test
+    sudo RAILS_ENV=#{node["deploy"]["cs2n"]["rails_env"]} bundle exec rake job_processess:test
   }
 end
