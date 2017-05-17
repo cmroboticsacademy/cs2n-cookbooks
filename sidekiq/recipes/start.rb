638e78b8d7d2b04f8e3247574e3d085d311661c2
Vu Nguyen <vnguyen@robomatter.com>
2017-05-17 15:37:47 -0400

    Adding a log

--- sidekiq/recipes/start.rb
+++ sidekiq/recipes/start.rb
@@ -12,7 +12,7 @@ node[:deploy].each do |application, deploy|
 
   execute "rake sidekiq:start" do
     user "#{deploy[:user]}"
-    command       "RAILS_ENV=#{rails_env} bundle exec sidekiq -d"
+    command       "RAILS_ENV=#{rails_env} bundle exec sidekiq -d -L ./log/sidekiq.log"
     cwd           release_path
   end
   execute "sudo chmod -R 776 #{release_path}" do
