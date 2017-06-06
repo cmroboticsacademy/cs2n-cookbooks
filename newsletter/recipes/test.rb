346c31490b93923f92560ad0b43d12968666583d
Vu Nguyen <vnguyen@robomatter.com>
2017-06-06 11:48:33 -0400

    Another method

--- newsletter/recipes/test.rb
+++ newsletter/recipes/test.rb
@@ -1,5 +1,6 @@
 execute 'rake ' do
-  command "sudo bundle exec rake job_processess:summer_of_learning_email[test]"
-  cwd node[:deploy]['cs2n'][:deploy_to]
-  environment 'RAILS_ENV' => node['deploy']['cs2n']['rails_env']
+  cwd "#{node[:deploy]['cs2n'][:deploy_to]}/current"
+  user "deploy"
+  environment ({"RAILS_ENV" => node[:deploy]['cs2n'][:rails_env]}) 
+  command "sudo /usr/local/bin/bundle exec rake job_processess:summer_of_learning_email[test]"
 end
