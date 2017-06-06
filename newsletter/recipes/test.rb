406432d028055f04c320e89239bda3e7d7815d44
Vu Nguyen <vnguyen@robomatter.com>
2017-06-06 11:09:50 -0400

    maybe adding in the environment

--- newsletter/recipes/test.rb
+++ newsletter/recipes/test.rb
@@ -2,7 +2,7 @@ script 'Run Test Newsletter' do
   interpreter "ruby"
   cwd node[:deploy]['cs2n'][:deploy_to]
   code <<-EOH
-    sudo bundle exec rails console
+    sudo RAILS_ENV=#{node[:deploy]['cs2n']["rails_env"]} bundle exec rails console
     WannaShareEmailsWorker.perform_async(:summer_of_learning,:test)
   EOH
 end
