3f3ea1340d886c4fcbb2b616ad9d06afbf0a6d5a
Vu Nguyen <vnguyen@robomatter.com>
2017-06-06 12:00:01 -0400

    adding environment to the command

--- newsletter/recipes/test.rb
+++ newsletter/recipes/test.rb
@@ -1,5 +1,5 @@
 execute 'Running rake job_processess:summer_of_learning_email[test]' do
   cwd "#{node[:deploy]['cs2n'][:deploy_to]}/current"
   environment ({"RAILS_ENV" => node[:deploy]['cs2n'][:rails_env]}) 
-  command "/usr/local/bin/bundle exec rake job_processess:summer_of_learning_email[test]"
+  command "/usr/local/bin/bundle exec rake job_processess:summer_of_learning_email[test] #{node[:deploy]['cs2n'][:rails_env]}"
 end
