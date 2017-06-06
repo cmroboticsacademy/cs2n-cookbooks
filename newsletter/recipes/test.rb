07c3a2e79ed6bc2bd7e46ae331b8e74034ceb913
Vu Nguyen <vnguyen@robomatter.com>
2017-06-06 11:52:16 -0400

    Removing the user

--- newsletter/recipes/test.rb
+++ newsletter/recipes/test.rb
@@ -1,6 +1,5 @@
 execute 'Running rake job_processess:summer_of_learning_email[test]' do
   cwd "#{node[:deploy]['cs2n'][:deploy_to]}/current"
-  user "deploy"
   environment ({"RAILS_ENV" => node[:deploy]['cs2n'][:rails_env]}) 
-  command "sudo /usr/local/bin/bundle exec rake job_processess:summer_of_learning_email[test]"
+  command "/usr/local/bin/bundle exec rake job_processess:summer_of_learning_email[test]"
 end
